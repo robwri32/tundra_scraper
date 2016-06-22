b0d3ce4fe16a11c52645e3ca35a4a54b777da782
robwri32 <robwri32@gmail.com>
2016-06-21 22:47:48 -0400

    added autotrader

---
+++ app-craigslist.js
@@ -0,0 +1,15 @@
+var Xray = require('x-ray');
+var x = Xray();
+
+
+
+// x-ray write to file
+x('http://atlanta.craigslist.org/search/sss?query=toyota+tundra&max_price=20000&min_auto_year=2008', '#sortable-results > div.rows > p', [{
+  title: '#titletextonly',
+  price: 'span.price',
+  permalink : 'span > span.pl > a@href',
+  image : {' > a > div.swipe > div > div:nth-child(1) > img[src]'}
+
+}])
+
+  .write('results.json');
--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var x = Xray();
 
 
 // x-ray write to file
-x('http://atlanta.craigslist.org/search/sss?query=toyota+tundra&max_price=20000&min_auto_year=2008', '#sortable-results > div.rows > p', [{
-  title: '#titletextonly',
-  price: 'span.price',
-  permalink : 'span > span.pl > a@href',
-  image : ' > a > div.swipe > div > div:nth-child(1) > img[src]'
+x('http://www.autotrader.com/cars-for-sale/cars+under+20000/Toyota/Tundra/Lawrenceville+GA-30043?endYear=2017&firstRecord=0&makeCode1=TOYOTA&maxPrice=20000&mmt=%5BTOYOTA%5BTUNDRA%5B%5D%5D%5B%5D%5D&modelCode1=TUNDRA&numRecords=100&searchRadius=200&showcaseListingId=429827767&showcaseOwnerId=588069&startYear=2006&Log=0', '.listing', [{
+  title: '#j_id_1_3ye_6 > h2 > a > span.atcui-truncate.ymm > span',
+  price: '.listing-price > .price > h4 > span',
+  permalink : '#j_id_1_3ye_6 > h2 > a@href',
+
 
 }])
 
-  .write('results.json');
+  .write('results-autotrader.json');
